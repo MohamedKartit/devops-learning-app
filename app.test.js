caf3bea70f6fc56ccbf09301a07ccb159521cbac
Mohamed Kartit <mkartit9@gmail.com>
2024-12-26 20:00:32 +0100

    Edit the vercel token

--- app.test.js
+++ app.test.js
@@ -1,15 +1,3 @@
-// const request = require('supertest');
-// const app = require('./app');
-
-// describe('GET /', () => {
-//   it('responds with HTML containing DevOps Learning App', async () => {
-//     const response = await request(app).get('/');
-//     expect(response.statusCode).toBe(200);
-//     expect(response.text).toContain('DevOps Learning App');
-//     expect(response.headers['content-type']).toMatch(/html/);
-//   });
-// });
-
 const request = require('supertest');
 const app = require('./app');
 
@@ -26,4 +14,3 @@ describe('GET /', () => {
     expect(response.statusCode).toBe(404);
   });
 });
-
