e54e41070552bba7914ec509310aea2872b9fc55
Mohamed Kartit <mkartit9@gmail.com>
2024-12-26 12:46:39 -0500

    Add small Front Part

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello, DevOps!' });
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -2,9 +2,10 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('GET /', () => {
-  it('responds with Hello, DevOps!', async () => {
+  it('responds with HTML containing DevOps Learning App', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ message: 'Hello, DevOps!' });
+    expect(response.text).toContain('DevOps Learning App');
+    expect(response.headers['content-type']).toMatch(/html/);
   });
 });
